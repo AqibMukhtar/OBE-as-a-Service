be02ef59c8382ba845ea13f86404d8cb16112648
Muhammad Uzair <muzair2810@gmail.com>
2021-02-23 07:09:37 -0800

    added addTeachers procedure

--- Database/MySQL Procedures/Muhammad_Uzair.sql
+++ Database/MySQL Procedures/Muhammad_Uzair.sql
@@ -36,3 +36,38 @@ BEGIN
 		end if;
 	end if;
 END
+
+
+
+
+
+
+
+
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `addTeachers`(program_id tinyint, batch_id smallint, section_name char(2), teacher_id mediumint, course_id smallint)
+BEGIN
+declare section_verification boolean default sectionIdVerify(program_id, batch_id, section_name);
+declare teacher_verification boolean default teacherIdVerify(teacher_id, program_id);
+declare batch_year tinyint default batchYear(batch_id);
+declare section_id smallint;
+declare record_exists boolean;
+declare is_completed boolean;
+set section_id = (select sectionId from section where programId = program_id AND batchId = batch_id AND sectionName = section_name);
+set record_exists = isSectionteacherCourseRecordExisting(section_id, teacher_id, course_id);
+set is_completed = (select isCompleted from sectionteachercoursejunction where sectionId = section_id and courseId = course_id);
+if batch_year < 0 or batch_year > 4 then
+    SELECT "you can only assign teachers to batches currently studying in University" AS "Message", FALSE AS "Success";
+elseif !teacher_verification then
+	SELECT "This teacher does not exist" AS "Message", FALSE AS "Success";
+elseif record_exists then
+	SELECT "This record already exists" AS "Message", FALSE AS "Success";
+elseif is_completed then
+	select "You cannot update this record" as "message", false as "Success";
+elseif !section_verification then
+	select "This section doesnot exist" as "message", false as "Success";
+else
+	UPDATE sectionteachercoursejunction SET teacherId = teacher_id WHERE sectionId = section_id AND courseId = course_id;
+	select "successfully updated record" as "message", true as "Success";
+end if;
+END
